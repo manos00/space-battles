70e807e8004f92b249b61726c4fb9af85b9fd4ff
manos00 <manos.wagner@gmail.com>
2021-03-20 19:35:04 +0100

    removed setup from gitignore

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import git
+import platform
+import winshell
+import win32com.client
+import pythoncom
+
+desktop = winshell.desktop()
+
+if platform.system() == 'Windows':
+    appdata = os.environ.get('appdata')
+    spacebattles = appdata+'/space-battles'
+    try:
+        git.Git(appdata).clone('https://github.com/manos00/space-battles')
+    except git.exc.GitCommandError as g:
+        if g:
+            pass
+    img = spacebattles+'/img'
+    db = spacebattles+'/highscores'
+    if os.path.exists(db):
+        pass
+    else:
+        os.mkdir(db)
+    path = os.path.join(desktop, 'SpaceBattles.lnk')
+    target = f"{spacebattles}/informatik.exe"
+    icon = f"{spacebattles}/img/spaceship.ico"
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.IconLocation = icon
+    shortcut.save()
+else:
+    try:
+        git.Git().clone('https://github.com/manos00/space-battles')
+    except git.exc.GitCommandError as g:
+        if g:
+            pass
+    img = '/img'
+    db = '/highscores'
+    if os.path.exists(db):
+        pass
+    else:
+        os.mkdir(db)
